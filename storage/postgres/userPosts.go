c043fe68d25c96a5171edde8181aabb925c46325
Khusniddin Nurmamatov <93917817+Khusniddin2004@users.noreply.github.com>
2022-04-13 14:57:52 +0500

    postgres.go to  userPosts.go

---
+++ storage/postgres/userPosts.go
@@ -0,0 +1,39 @@
+package postgres
+
+import (
+	"fmt"
+	"log"
+	pp "tasks/Instagram_clone/insta_post/genproto/post_proto"
+)
+
+func (r *PostRepo) UserPostsList(req *pp.ListPostsReq) (*pp.ListPostsRes, error) {
+
+	var (
+		resList pp.ListPostsRes
+	)
+	queryUser := `SELECT post_id FROM post WHERE user_id=$1`
+	rows, err := r.db.Query(queryUser, req.UserId)
+	if err != nil {
+		log.Println("Error while Users posts list", err)
+		return nil, err
+	}
+	
+	for rows.Next() {
+		postId := ""
+		err = rows.Scan(
+			&postId,
+		)
+		if err != nil {
+			log.Println("Error while take postsId with userId", err)
+			return nil, err
+		}
+		res, err := r.GetPost(postId)
+		if err != nil {
+			fmt.Println("Error while get user in list method", err)
+			return nil, err
+		}
+		resList.Posts = append(resList.Posts, res)
+	}
+	
+	return &resList, nil
+}
